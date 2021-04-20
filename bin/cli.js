534c63b08b48404aee5f70b347efd4f45e52412d
Keith Broughton <hello@keithbro.com>
2021-04-20 20:51:08 +1000

    Dependencies

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,7 @@
+#!/usr/bin/env node
+
+"use strict";
+
 const path = require("path");
 const fs = require("fs");
 const openapiTS = require("openapi-typescript").default;
@@ -7,7 +11,16 @@ const buildRouter = require("../dist/buildRouter").default;
 const spec = "./openapi.json";
 const types = "./src/generated/openapi.ts";
 
-const schemasFile = path.resolve(process.cwd(), process.argv[2]);
+const relativeSchemasFile = process.argv[2];
+const outPath = process.argv[3];
+
+if (!relativeSchemasFile || !outPath) console.log(usage());
+
+const schemasFile = path.resolve(process.cwd(), relativeSchemasFile);
+if (!fs.existsSync(schemasFile)) {
+  console.log(`Could not find file: ${relativeSchemasFile}`);
+  process.exit(1);
+}
 
 const schemas = require(schemasFile);
 const router = buildRouter(schemas);
@@ -21,5 +34,10 @@ fs.writeFileSync(types, swaggerTS);
 
 generate({
   input: swagger,
-  output: "./src/generated",
+  output: outPath,
 });
+
+function usage() {
+  console.log("Usage: yarn openapi schema.js src/generated");
+  process.exit(1);
+}
